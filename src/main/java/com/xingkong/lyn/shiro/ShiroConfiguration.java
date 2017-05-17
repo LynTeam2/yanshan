aca6485f1a2d7ea240ec558c112f18910377f8a5
lyn <632316944@qq.com>
2017-05-17 15:41:25 +0800

    添加记住我功能

--- src/main/java/com/xingkong/lyn/shiro/ShiroConfiguration.java
+++ src/main/java/com/xingkong/lyn/shiro/ShiroConfiguration.java
@@ -4,7 +4,9 @@ import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -57,6 +59,10 @@ public class ShiroConfiguration {
         //配置退出过滤器，其中具体的退出代码Shiro已经实现
         filterChainDefinitionMap.put("/logout","logout");
 
+        //配置记住我或认证通过可以访问的地址
+        filterChainDefinitionMap.put("/index", "user");
+        filterChainDefinitionMap.put("/", "user");
+
         //<!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
         //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
         filterChainDefinitionMap.put("/**","authc");
@@ -77,7 +83,10 @@ public class ShiroConfiguration {
     @Bean
     public SecurityManager securityManager(){
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        //设置realm
         securityManager.setRealm(myShiroRealm());
+        //注入记住我管理器
+        securityManager.setRememberMeManager(rememberMeManager());
         return securityManager;
     }
 
@@ -106,5 +115,30 @@ public class ShiroConfiguration {
                 AuthorizationAttributeSourceAdvisor();
         authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
         return authorizationAttributeSourceAdvisor;
+    }
+
+    /**
+     * 添加记住我功能
+     * cookie管理对象
+     */
+    @Bean
+    public SimpleCookie rememberMeCookie(){
+        System.out.println("ShiroConfiguration.rememberMeCookie()");
+        //这个参数是cookie的名称，对应前端的checkbox的name = rememberMe
+        SimpleCookie simpleCookie = new SimpleCookie("rememberMe");
+        //<!-- 记住我cookie生效时间30天 ,单位秒;-->
+        simpleCookie.setMaxAge(259200);
+        return simpleCookie;
+    }
+
+    /**
+     * cookie管理对象
+     */
+    @Bean
+    public CookieRememberMeManager rememberMeManager(){
+        System.out.println("ShiroConfiguration.rememberMeManager()");
+        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+        cookieRememberMeManager.setCookie(rememberMeCookie());
+        return cookieRememberMeManager;
     }
 }
