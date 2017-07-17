1f2f3c7fda14ad5dc2da8a398f7ce73deb3a2d63
lyn <632316944@qq.com>
2017-07-17 16:11:25 +0800

    兼容一尚红功能修改

--- src/main/java/com/xingkong/lyn/comment/SimpleCORSFilter.java
+++ src/main/java/com/xingkong/lyn/comment/SimpleCORSFilter.java
@@ -22,7 +22,7 @@ public class SimpleCORSFilter  implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse resp = (HttpServletResponse) response;
-        resp.setHeader("Access-Control-Allow-Origin", "192.168.0.5");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
         resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         resp.setHeader("Access-Control-Max-Age", "3600");
         resp.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type");
